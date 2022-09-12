9470c29897ac95b80e462c31ee7e02c0cb12deb4
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-09-12 18:47:29 +0530

    LL2

--- LinkedList/questions.cpp
+++ LinkedList/questions.cpp
@@ -376,6 +376,57 @@ bool hasCycle(ListNode *head) {
         return false;
     }
 
+// leetcode 142 ==================================================== 
+
+ListNode *detectCycle(ListNode *head) {
+    if(head==nullptr || head->next==nullptr){
+        return nullptr;
+    }
+
+    ListNode* slow=head;
+    ListNode* fast=head;
+
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        
+        if(slow==fast) break;
+    } 
+
+    if(slow!=fast) return nullptr;
+
+    fast=head;
+    while(slow!=fast){
+        slow=slow->next;
+        fast=fast->next;
+    }
+
+    return slow;
+}
+
+// leetcode 160 =================================================================
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    if(headA == nullptr || headB==nullptr){
+        return nullptr;
+    }        
+
+    ListNode* tail=headA;
+    ListNode* curr=headA;
+
+    while(curr){
+        tail=curr;
+        curr=curr->next;
+    }
+
+    tail->next=headB;
+
+    ListNode* ans=detectCycle(headA);
+
+    tail->next=nullptr;
+    return ans;
+}
+
+
 int main(){
 
 }
